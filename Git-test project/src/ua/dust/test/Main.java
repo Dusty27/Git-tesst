0f9df9c55c562607249ec72c06b18cf8b0f33de0
Администратор <@195.242.81.60>
2015-02-03 21:48:48 +0200

    !!!

--- Git-test project/src/ua/dust/test/Main.java
+++ Git-test project/src/ua/dust/test/Main.java
@@ -6,7 +6,7 @@ package ua.dust.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!!!");
+		System.out.println("Hello from Git Hub!");
 	}
 }
    
